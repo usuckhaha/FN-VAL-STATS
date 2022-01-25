b27a87384699ca1cf34c60df1db307d51e299b31
Angel <97680804+usuckhaha@users.noreply.github.com>
2022-01-25 10:12:53 -0500

    Add files via upload
    aaaaaa object

--- src/FnStats.java
+++ src/FnStats.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class FnStats {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
+        Player dylan = new Player();
         String gum = "";
         System.out.println("Welcome to FN-VAL-STATS, where we get the happiness of your kd to the reach of your in-game tag");
         while(!gum.equals("quit")){
             System.out.print("Lemme take a good look at your gamer tag, gamer boi: ");
             gum = scan.nextLine();
+            dylan.setTag(gum);
             //analyzer stuff
             System.out.println("*work done*");
         }
