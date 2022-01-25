1f9a3705ce74ca154dc1873b2fd4569d6d02be49
Angel <97680804+usuckhaha@users.noreply.github.com>
2022-01-25 09:43:19 -0500

    Add files via upload
    Angel's

---
+++ src/FnStats.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FnStats {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String gum = "";
+        System.out.println("Welcome to FN-VAL-STATS, where we get the happiness of your kd to the reach of your in-game tag");
+        while(!gum.equals("quit")){
+            System.out.print("Lemme take a good look at your gamer tag, gamer boi: ");
+            gum = scan.nextLine();
+            //analyzer stuff
+            System.out.println("*work done*");
+        }
+
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,12 @@
+public class Player {
+    private String tag="";
+    public Player(String tag){
+        this.tag = tag;
+    }
+    public void setTag(String tag){
+        this.tag = tag;
+    }
+    public String getTag(){
+        return this.tag;
+    }
+}
