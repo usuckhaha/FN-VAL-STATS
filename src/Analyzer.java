58639c5c70bdc06e48978edba02f73111de2af9b
Angel <97680804+usuckhaha@users.noreply.github.com>
2022-01-27 12:01:25 -0500

    Add files via upload
    stringz analyzer

--- src/Analyzer.java
+++ src/Analyzer.java
@@ -1,68 +1,51 @@
-import java.net.*;
-import java.io.*;
-import java.util.Scanner;
-import java.nio.charset.*;
-import org.json.*;
+        import java.net.*;
+        import java.io.*;
 
 
 public class Analyzer{
-   
-   public static String wins(String ign){
-   
-  
-    
-    try{
-      
-     String website = "https://fortnitetracker.com/profile/all/" + ign;
-     
-     URL tracker = new URL(website);
-     URLConnection yc = tracker.openConnection();
-     BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));     
-      String inputLine;
-      String line = "";
-     
-    while ((inputLine = in.readLine()) != null)
-      		      line+=inputLine;
-                         System.out.println(line+ "AHAAAAA");
 
-        			in.close();
-        			int pos=0;
-	                    
-			        pos = line.indexOf("'metadata': {'key': 'Top1', 'name': 'Wins','categoryKey': 'tops','categoryName': 'Tops','isReversed': false},'value': ", pos+1);
-      			  if(pos>-1)
-        			{
-          			 //int endbdayIndex = line.indexOf("'metadata': {'key': 'Top1', 'name': 'Wins','categoryKey': 'tops','categoryName': 'Tops','isReversed': false},'value': ");
-            
-          				String wins = line.substring(pos+211, pos+215);
-          				return "Wins: " + wins ;                
-			    		}
-    					else
-    						return "We don't know";
-                     
-             
-			  	}
-			    catch(FileNotFoundException ex)
-			  {
-			  	return "unknown";
-			  }
-			  catch(MalformedURLException e)
-			  {
-			  	return "badly formed url exception occurred";
-			  	
-			  }
-			   catch(IOException e)
-			  {
-			  	return "IO exception occurred";
-			 
-			  }
-   
-   }
-    
-   
- }
-
-			  
-           	
-			  
+    public static String wins(String ign){
 
 
+
+        try{
+
+            String website = "https://fortnitetracker.com/profile/all/" + ign;
+
+            URL tracker = new URL(website);
+            URLConnection yc = tracker.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+            String inputLine;
+            String line = "";
+            while ((inputLine = in.readLine()) != null) {
+                line = line.concat(inputLine);
+            }
+            in.close();
+
+            line = line.substring(line.indexOf("<script type=\"text/javascript\">var imp_data = {")+31,line.indexOf("<script type=\"text/x-template\" id=\"trn-profile-header-favorite-template\">")-9);
+            String json = line.substring(line.indexOf("stats")+5);
+            json = json.substring(json.indexOf("stats")+5);
+            json = json.substring(json.indexOf("all"));
+
+
+
+        }
+        catch(FileNotFoundException ex)
+        {
+            return "unknown";
+        }
+        catch(MalformedURLException e)
+        {
+            return "badly formed url exception occurred";
+
+        }
+        catch(IOException e)
+        {
+            return "IO exception occurred";
+
+        }
+
+    }
+
+
+}
